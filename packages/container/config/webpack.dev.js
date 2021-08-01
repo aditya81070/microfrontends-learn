34e123991abb345b0a6c4ff89a17b925a1bd2f43
Aditya Agarwal <aditya.agarwal@peak.ai>
2021-08-01 17:16:54 +0530

    connect container and marketing app

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,9 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common');
+
 const devConfig = {
   mode: 'development',
   devServer: {
@@ -11,6 +13,13 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      filename: 'remoteEntry.js',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { mount as mountMarketing } from 'marketing/MarketingApp';
+
+console.log(mountMarketing);
 export default function App() {
   return <h1>Hello from container</h1>;
 }
