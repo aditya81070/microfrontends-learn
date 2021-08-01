4bdadf250c98108e778bc7ffa45e3c4bc3edf19e
Aditya Agarwal <aditya.agarwal@peak.ai>
2021-08-01 16:29:24 +0530

    export marketing mount function

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,1 +1,15 @@
-console.log('hello from marketing');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mount = (el) => {
+  ReactDOM.render(<h1>Hii There from marketing</h1>, el);
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
