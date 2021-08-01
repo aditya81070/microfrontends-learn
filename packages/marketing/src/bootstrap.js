f2cac1e6ef8131492577f13bb3a089a1982d1073
Aditya Agarwal <aditya.agarwal@peak.ai>
2021-08-01 16:57:12 +0530

    add Landing and Pricing page to marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
 const mount = (el) => {
-  ReactDOM.render(<h1>Hii There from marketing</h1>, el);
+  ReactDOM.render(<App />, el);
 };
 
 if (process.env.NODE_ENV === 'development') {
