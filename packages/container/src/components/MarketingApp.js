cc4d1b9305b11a24627ce5f098eb746dbc361e21
Aditya Agarwal <aditya.agarwal@peak.ai>
2021-08-03 09:10:34 +0530

    communicate marketing route changes to container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,19 @@
 import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
-
+import { useHistory, useLocation } from 'react-router-dom';
 export default function MarketingApp() {
   const appRoot = useRef();
-
+  const history = useHistory();
+  const { pathname } = useLocation();
   useEffect(() => {
-    mount(appRoot.current);
+    mount(appRoot.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        console.log('container noticed navigation in marketing');
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
   }, []);
   return <div ref={appRoot} />;
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 
 import App from './App';
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate } = {}) => {
+  const history = createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === 'development') {
